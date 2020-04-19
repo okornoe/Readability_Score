1e9c0a9782a5986e3155dd5ff153997a517df4f9
okornoe <kelvin.okornoe@gmail.com>
2020-04-19 23:09:28 +0100

    refactored the switch statement into a method

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,57 +1,71 @@
 package readability;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    static String ageRange;
+
     public static void main(String[] args) throws IOException {
         // use args[0] to store the file name and append it to the path
         //String path = args[0];
-        String path = "/moocs/jetbrains/Readability Score/in2.txt";
-        String sentence;
-        int sentenceCount = 0;
-        int wordCount = 0;
-        int charCount = 0;
-        String ageRange = "";
+
+        int numOfSentence = 0;
+        int numOfWords = 0;
+        int numOfCharacters = 0;
+        int roundedIntScore;
+        float numCharDivideNumWords;
+        float numWordsDivideNumSentence;
+        double score;
+
+
         final float constVal4_71 = 4.71f;
         final float constVal0_5 = 0.5f;
         final float constVal321_43 = 21.43f;
+
+        String path = "/moocs/jetbrains/Readability Score/in.txt";
         File file = new File(path);
         Scanner scanner = new Scanner(file);
         //System.out.println(file.exists());
 
         while (scanner.hasNextLine()) {
-            sentence = scanner.nextLine();
-            String[] strArr = sentence.split("[!?.]");
-            sentenceCount = sentenceCount + strArr.length;
+            String readLine = scanner.nextLine();
+            String[] sentence = readLine.split("[!?.]");
+            numOfSentence = numOfSentence + sentence.length;
 
-            String[] ser = sentence.split("\\s+");
-            wordCount = wordCount + ser.length;
-            for (String i : ser) {
-                charCount = charCount + i.length();
+            String[] words = readLine.split("\\s+");
+            numOfWords = numOfWords + words.length;
+            for (String word : words) {
+                numOfCharacters = numOfCharacters + word.length();
             }
         }
 
-        float charDivWords =  (float)charCount/wordCount;
-        float wordsDivSentences = (float) wordCount/sentenceCount;
+        //calculates the score of the text.
+        numCharDivideNumWords = (float) numOfCharacters / numOfWords;
+        numWordsDivideNumSentence = (float) numOfWords / numOfSentence;
+        score = (constVal4_71 * numCharDivideNumWords) + ((constVal0_5 * numWordsDivideNumSentence) - constVal321_43);
 
-        double score = (constVal4_71 * charDivWords) + ((constVal0_5 * wordsDivSentences) - constVal321_43);
+        roundedIntScore = (int) Math.ceil(score);
+        readableAge(roundedIntScore);
 
-
-        System.out.println("Words: " + wordCount);
-        System.out.println("Sentences: " + sentenceCount);
-        System.out.println("Characters: " + charCount);
+        System.out.println("Words: " + numOfWords);
+        System.out.println("Sentences: " + numOfSentence);
+        System.out.println("Characters: " + numOfCharacters);
         System.out.print("The score is: ");
-        System.out.format("%.2f",score);
-       System.out.println();
-        int switchScore = (int)Math.ceil(score);
+        System.out.format("%.2f", score);
+        System.out.println();
+        System.out.println("This text should be understood by " + ageRange + " year olds.");
+    }
 
-        switch (switchScore) {
+    //This method determines which age range will be able to read the text.
+    private static void readableAge(int score) {
+        switch (score) {
             case 1:
-                 ageRange = "5-6";
+                ageRange = "5-6";
                 break;
             case 2:
-                 ageRange =  "6-7";
+                ageRange = "6-7";
                 break;
             case 3:
                 ageRange = "7-9";
@@ -90,10 +104,9 @@ public class Main {
                 ageRange = "24+";
                 break;
             default:
-                 ageRange = "Unknown";
+                ageRange = "Unknown";
                 break;
         }
-        System.out.println("This text should be understood by " + ageRange + " year olds.");
     }
 }
 

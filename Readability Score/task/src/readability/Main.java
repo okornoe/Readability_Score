a96dd88f5ac84b5e984c0067150c2b48f52492be
okornoe <kelvin.okornoe@gmail.com>
2020-04-25 13:45:09 +0000

    clean up code

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -7,28 +7,21 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *
- * double floor = Math.floor(3.78); // floor is 3.0
- * double ceil = Math.ceil(4.15); // ceil is 5.0
- *
- * Math.floor(...) returns the largest double value that is less than or
- * equal to its argument and is equal to an integer;
- *
- * Math.ceil(...) returns the smallest double value that is greater than or
- * equal to its argument and is equal to an integer.
- */
-
 public class Main {
 
     static String str = "";
     static String path = "/moocs/jetbrains/Readability Score/newTest.txt";
+
     public static void main(String[] args) throws IOException {
         //str = readFile(args[0]); // uncomment this line when you want pass the file name from the console
         str = readFile(path);  // use this when you want to pass the file path to the program directly.
 
         print();
+    }
 
+    //reads the file
+    private static String readFile(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
     }
 
     private static void print() {
@@ -38,14 +31,16 @@ public class Main {
         System.out.println("Syllables: " + syllables());
         System.out.println("Polysyllables: " + polySyllables());
         scoreSelection();
+        System.out.println();
         averageReadableAge();
     }
 
-    private static void printARI(){
+    private static void printARI() {
         String readabilityType = "Automated Readability Index: ";
         double scoreValue = authomaticReadabilityIndex();
         int readableAge = readableAge(Math.round(scoreValue));
         System.out.println(readabilityType + scoreValue + " (about " + readableAge + " year olds).");
+        System.out.println();
     }
 
     private static void printFK() {
@@ -53,6 +48,7 @@ public class Main {
         double scoreValue = fleschKincaidReadTest();
         int readableAge = readableAge(Math.round(scoreValue));
         System.out.println(readabilityType + scoreValue + " (about " + readableAge + " year olds).");
+        System.out.println();
     }
 
     private static void printSMOG() {
@@ -60,6 +56,7 @@ public class Main {
         double scoreValue = simpleMeasureOfGobbledygook();
         int readableAge = readableAge(Math.round(scoreValue));
         System.out.println(readabilityType + scoreValue + " (about " + readableAge + " year olds).");
+        System.out.println();
     }
 
     private static void printCL() {
@@ -67,6 +64,7 @@ public class Main {
         double scoreValue = colemanLauIndex();
         int readableAge = readableAge(Math.round(scoreValue));
         System.out.println(readabilityType + scoreValue + " (about " + readableAge + " year olds).");
+        //System.out.println();
     }
 
     private static void printAll() {
@@ -79,6 +77,7 @@ public class Main {
     private static void scoreSelection() {
         System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all):");
         String scoreType = new Scanner(System.in).next();
+        System.out.println();
 
         switch (scoreType) {
             case "ARI":
@@ -102,8 +101,26 @@ public class Main {
         }
     }
 
+    //calculates the number of sentences in the file
+    private static int numberOfSentences() {
+        String[] s = str.replaceAll("\\s+", "").split("[!?.]");
+        return s.length;
+    }
+
+    //calculates the number of words in the file
+    private static int numberOfWords() {
+        String[] words = str.split("\\s+");
+        return words.length;
+    }
+
+    //calculates the number characters in the file
+    private static int numberOfCharacters() {
+        String words = str.replaceAll("\\s+", "");
+        return words.length();
+    }
+
     //calculates the number syllables in a word.
-    private static int syllables(){
+    private static int syllables() {
         String[] words = str.split("\\s+");
         int sum = 0;
         for (String word : words) {
@@ -120,7 +137,7 @@ public class Main {
         String[] words = str.split("\\s+");
         for (String word : words) {
             int numOfVowelsInWord = countVowelsInWord(word);
-            if (numOfVowelsInWord > 2 ) {
+            if (numOfVowelsInWord > 2) {
                 pSyllable = pSyllable + 1;
             }
         }
@@ -132,7 +149,7 @@ public class Main {
         final Pattern p = Pattern.compile("([ayeiou]+)");
         String lowerCase = word.toLowerCase();
         if (lowerCase.endsWith(".") || lowerCase.endsWith("!") || lowerCase.endsWith("?") || lowerCase.endsWith(",")) {
-            lowerCase = lowerCase.substring(0,lowerCase.length()-1);
+            lowerCase = lowerCase.substring(0, lowerCase.length() - 1);
         }
         final Matcher m = p.matcher(lowerCase);
         int count = 0;
@@ -145,48 +162,12 @@ public class Main {
 
     //calculate the average readable age
     private static void averageReadableAge() {
-        double averageAge = (readableAge((int)Math.round(authomaticReadabilityIndex())) +
-                 readableAge((int)Math.round(fleschKincaidReadTest())) +
-                 readableAge((int)Math.round(simpleMeasureOfGobbledygook())) +
-                 readableAge((int)Math.round(colemanLauIndex())))/4.0;
-        System.out.println("This text should be understood in average by " + averageAge +
+        double averageAge = (readableAge((int) Math.round(authomaticReadabilityIndex())) +
+                readableAge((int) Math.round(fleschKincaidReadTest())) +
+                readableAge((int) Math.round(simpleMeasureOfGobbledygook())) +
+                readableAge((int) Math.round(colemanLauIndex()))) / 4.0;
+                System.out.println("This text should be understood in average by " + averageAge +
                 " year olds.");
-    }
-
-
-    //calculates the Simple Measure Of Gobbledygook.
-    private static double simpleMeasureOfGobbledygook() {
-        return Math.round((1.043 * Math.sqrt(polySyllables() * ((double)30 / numberOfSentences())) + 3.1291) * 100.0 )/100.0;
-    }
-
-    //calculates the coleman-Lau Index
-    private static double colemanLauIndex () {
-        double l = ((double) numberOfCharacters() / (double) numberOfWords()) * 100;
-        double s = (double) numberOfSentences() / (double) numberOfWords() * 100;
-        return Math.round((0.0588 * l - 0.296 * s - 15.8) * 100.0) / 100.0;
-    }
-
-    //reads the file
-    private static String readFile(String fileName) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(fileName)));
-    }
-
-    //calculates the number of sentences in the file
-    private static int numberOfSentences()  {
-        String[] s = str.replaceAll("\\s+", "").split("[!?.]");
-        return s.length;
-    }
-
-    //calculates the number of words in the file
-    private static int numberOfWords(){
-        String[] words = str.split("\\s+");
-        return words.length;
-    }
-
-    //calculates the number characters in the file
-    private static int numberOfCharacters()  {
-        String words = str.replaceAll("\\s+", "");
-        return words.length();
     }
 
     //calculates the score of the text.
@@ -198,13 +179,33 @@ public class Main {
         double numWordsDivideNumSentence;
         numCharDivideNumWords = (double) numberOfCharacters() / numberOfWords();
         numWordsDivideNumSentence = (double) numberOfWords() / numberOfSentences();
-        return  Math.round((((constVal4_71 * numCharDivideNumWords) + ((constVal0_5 * numWordsDivideNumSentence) - constVal321_43)) * 100.0))/100.0;
+        return Math.round((((constVal4_71 * numCharDivideNumWords) +
+                ((constVal0_5 * numWordsDivideNumSentence) - constVal321_43)) * 100.0)) / 100.0;
+    }
+
+    //calculates the Flesch Kincaid score.
+    private static double fleschKincaidReadTest() {
+        return Math.round((0.39 * ((double) numberOfWords() / numberOfSentences()) + 11.8 *
+                ((double) syllables() / numberOfWords()) - 15.59) * 100.0) / 100.0;
+    }
+
+    //calculates the Simple Measure Of Gobbledygook.
+    private static double simpleMeasureOfGobbledygook() {
+        return Math.round((1.043 * Math.sqrt(polySyllables() * ((double) 30 / numberOfSentences()))
+                + 3.1291) * 100.0) / 100.0;
+    }
+
+    //calculates the coleman-Lau Index
+    private static double colemanLauIndex() {
+        double l = ((double) numberOfCharacters() / (double) numberOfWords()) * 100;
+        double s = (double) numberOfSentences() / (double) numberOfWords() * 100;
+        return Math.round((0.0588 * l - 0.296 * s - 15.8) * 100.0) / 100.0;
     }
 
     //work to be done here to return maximum age.
     //This method determines which age range will be able to read the text.
     private static int readableAge(long score) {
-        switch ((int)score) {
+        switch ((int) score) {
             case 1:
                 return 6;
             case 2:
@@ -236,11 +237,6 @@ public class Main {
             default:
                 return 0;
         }
-    }
-
-    //calculates the Flesch Kincaid score.
-    private static double fleschKincaidReadTest(){
-        return  Math.round((0.39 * ((double) numberOfWords()/numberOfSentences()) + 11.8 *  ((double) syllables()/numberOfWords()) - 15.59) *100.0)/100.0;
     }
 }
 
